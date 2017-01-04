f57760a3d350cfb3d399ce9eaef8156af1537570
Ed Ilyin <ed.il@live.com>
2017-01-04 12:30:39 +0200

    adds cmdlet

--- src/FunFun/FunFun.fs
+++ src/FunFun/FunFun.fs
@@ -1,4 +1,14 @@
 namespace FunFun
 
-type FunFun() = 
-    member this.X = "F#"
+open System.Management.Automation
+
+
+[<Cmdlet(VerbsCommon.Get,"Fun")>]
+type FortyTwoCmdlet() = 
+    inherit PSCmdlet()
+
+    [<Parameter(Mandatory = true, Position = 0)>]
+    member val Radius : float = 0.0 with get, set
+
+    override this.ProcessRecord() =
+       System.Math.PI * pown this.Radius 2 |> this.WriteObject
