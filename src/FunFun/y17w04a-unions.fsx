f1508f40d8c51015058fc0e4ef7bcfacf25a18b6
Ed Ilyin <ed.il@live.com>
2017-01-24 11:10:10 +0200

    y17w04a - unions

---
+++ src/FunFun/y17w04a-unions.fsx
@@ -0,0 +1,40 @@
+type Koordinata<'T> = {
+    x: 'T
+    y: 'T
+}
+
+
+type Figura<'T> =
+    | Priamougolnik of ugol1: Koordinata<'T> * ugol2: Koordinata<'T>
+    | Krug of centr: Koordinata<'T> * radius: 'T
+
+
+let curry f x y = f (x, y)
+
+
+let sdvigVPravo sdvig objekt =
+    match objekt with
+        | Krug (koordinata, radius) ->
+            curry Krug
+                { koordinata with x = koordinata.x + sdvig }
+                radius
+
+        | Priamougolnik (u1, u2) ->
+            curry Priamougolnik
+                { u1 with x = u1.x + sdvig }
+                { u2 with x = u2.x + sdvig }
+
+
+let kruzhok = Krug ({x = 2.0; y = 23.2}, 50.2)
+
+
+sdvigVPravo 42.0 kruzhok
+
+type Result<'TSuccess, 'TFailure> =
+    | Success of 'TSuccess
+    | Failure of 'TFailure
+
+let tryGetFromServe x = Failure "Server sdoh"
+
+
+let a = 5 + 6
